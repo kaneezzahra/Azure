e3fe749087d2697ab08c61c2eb129cfe71a131bb
Kaneez zahra <zahra.kaneez07@gmail.com>
2021-08-23 11:04:00 +0530

    commiting..

---
+++ src/main/java/com/azure/Azuredemo/AzureDemoApplication.java
@@ -0,0 +1,14 @@
+package com.azure.Azuredemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzureDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzureDemoApplication.class, args);
+		System.out.println("This is demo app");
+	}
+
+}
